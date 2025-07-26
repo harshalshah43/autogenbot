ad3a35c3f5a69ac4de2bb7c60c4673c331929b0d
Harshal Shah <65274384+harshalshah43@users.noreply.github.com>
2025-07-26 16:30:57 +0530

    Update setup.py
    Update

--- rfqbot/setup.py
+++ rfqbot/setup.py
@@ -36,12 +36,11 @@ agent1 = AssistantAgent(
     description="A friendly AI agent that files user complaints.",
     system_message=(
         "You are a helpful assistant for customers of a logistics company"
-        "You must collect following shipping information:- port of loading, port of delivery, pickup address, delivery address and package summary."
-        "You can use tools to make port suggestions"
+        "You must collect following details:- port of loading, port of delivery, pickup address, delivery address and package summary."
+        "If user is not sure of port names, offer port suggestions by making a tool call and extract and provide names from the tool output for the user to choose"
         "You must also collect customer information:- Name,Company Name, Contact Number, Email id"
-        "Once the information is collected, generate a random 4-digit number as the RFQ ID."
-        "Customer information is mandatory, shipping information is not. Do not generate RFQ id unless customer information is collected."
-        "thank the user, and end the conversation. Do not continue the conversation after collecting all details."
+        "Once the information is collected, generate a random 4-digit number as the RFQ ID, Do not generate RFQ id unless customer information is collected."
+        "thank the user, inform the user that he/she will be contacted soon, and end the conversation. Do not continue the conversation after collecting all details."
     ),
     tools = [find_ports_tool,nearest_port_tool]
 )
